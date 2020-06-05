1905ae7a09e2bab8b28d0b6420e89f9a68e40c89
Pedro Meirelles <stalkersmurf00@gmail.com>
2020-06-05 06:04:50 -0300

    commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 var ibmdb = require("ibm_db");
 let connStr =
   "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-08.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=znq46101;PWD=7hwh1gp76cdz^4qn;";
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/ibmWatson", ibmWatsonRouter);
